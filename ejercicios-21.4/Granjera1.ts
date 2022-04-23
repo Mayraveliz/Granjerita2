1d5ef02d9369e3b9d5443dbf3b191adf8b4ccbec
Mayraveliz <103957168+Mayraveliz@users.noreply.github.com>
2022-04-23 20:45:27 -0300

    Initial commit

---
+++ ejercicios-21.4/Granjera1.ts
@@ -0,0 +1,7 @@
+console.log("repetir(4)");
+console.log("avanza");
+console.log("vaciar1");
+console.log("girar a la izquierda 90°");
+console.log("repetir(6)");
+console.log("avanzar");
+console.log("vaciar1");
---
+++ ejercicios-21.4/Granjera2.ts
@@ -0,0 +1,8 @@
+console.log("avanzar");
+console.log("llenar1");
+console.log("avanzar3");
+console.log("llenar1");
+console.log("avanzar");
+console.log("vaciar1");
+console.log("avanzar");
+console.log("vaciar1");
